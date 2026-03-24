fb24c429fa446f1d495b8b45b9cb2c05cc4da23a
916RasnitaRadu <rasnitaradu@gmail.com>
2026-03-24 12:46:30 +0200

    project init

---
+++ src/main/java/model/Actor.java
@@ -0,0 +1,40 @@
+package model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "actor", schema = "sakila")
+public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "actor_id", nullable = false)
+    private Short id;
+
+    @Size(max = 45)
+    @NotNull
+    @Column(name = "first_name", nullable = false, length = 45)
+    private String firstName;
+
+    @Size(max = 45)
+    @NotNull
+    @Column(name = "last_name", nullable = false, length = 45)
+    private String lastName;
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "last_update", nullable = false)
+    private Instant lastUpdate;
+
+    @ManyToMany(mappedBy = "actors")
+    private Set<Film> films = new HashSet<>();
+}
---
+++ src/main/java/model/Film.java
@@ -0,0 +1,74 @@
+package model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "film", schema = "sakila")
+public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "film_id", nullable = false)
+    private Short id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "release_year")
+    private Integer releaseYear;
+
+    @NotNull
+    @ColumnDefault("3")
+    @Column(name = "rental_duration", nullable = false)
+    private Short rentalDuration;
+
+    @NotNull
+    @ColumnDefault("4.99")
+    @Column(name = "rental_rate", nullable = false, precision = 4, scale = 2)
+    private BigDecimal rentalRate;
+
+    @Column(name = "length")
+    private Short length;
+
+    @NotNull
+    @ColumnDefault("19.99")
+    @Column(name = "replacement_cost", nullable = false, precision = 5, scale = 2)
+    private BigDecimal replacementCost;
+
+    @ColumnDefault("'G'")
+    @Lob
+    @Column(name = "rating", columnDefinition = "enum('G', 'PG', 'PG-13', 'R', 'NC-17')")
+    private String rating;
+
+    @Lob
+    @Column(name = "special_features", columnDefinition = "enum('Trailers', 'Commentaries', 'Deleted Scenes', 'Behind the Scenes'")
+    private String specialFeatures;
+
+    @NotNull
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "last_update", nullable = false)
+    private Instant lastUpdate;
+
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(
+            name = "film_actor",
+            joinColumns = { @JoinColumn(name = "film_id") },
+            inverseJoinColumns = { @JoinColumn(name = "actor_id") }
+    )
+    private List<Actor> actors = new ArrayList<>();
+}
